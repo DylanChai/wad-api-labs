41fc4486cdece50bd4551641f4ecfb6d7ed30b79
Dylan <dylanchai2003@gmail.com>
2023-12-16 09:58:49 +0000

    Mongo lab Excercises done

--- tasky-api/api/users/index.js
+++ tasky-api/api/users/index.js
@@ -10,23 +10,28 @@ router.get('/', async (req, res) => {
 });
 
 // register(Create)/Authenticate User
-router.post('/', async (req, res) => {
-    if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successful created new user.',
-        });
-    }
-    else {  //Must be an authenticate then!!! Query the DB and check if there's a match
-        const user = await User.findOne(req.body);
-        if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-        }else{
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+router.post('/', async (req, res, next) => {
+    try {
+        if (req.query.action === 'register') {  // if action is 'register' then save to DB
+            await User(req.body).save();
+            res.status(201).json({
+                code: 201,
+                msg: 'Successfully created new user.',
+            });
+        } else {  // Must be an authenticate then!!! Query the DB and check if there's a match
+            const user = await User.findOne(req.body);
+            if (!user) {
+                return res.status(401).json({ code: 401, msg: 'Authentication failed' });
+            } else {
+                return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+            }
         }
+    } catch (error) {
+        // Pass the error to the error-handling middleware
+        next(error);
     }
 });
+
 
 // Update a user
 router.put('/:id', async (req, res) => {
