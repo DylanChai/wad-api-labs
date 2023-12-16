9d7be8d4323a07d8ad26f468b4814becbc15f175
Dylan <dylanchai2003@gmail.com>
2023-12-16 13:03:56 +0000

    Upcoming Movies from TMDB

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
-import moviesRouter from './api/movies';   //import movies router
+import moviesRouter from './api/movies';
 
 dotenv.config();
 
@@ -14,9 +14,8 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
-app.use(defaultErrHandler);
 app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
-
+app.use(defaultErrHandler);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
