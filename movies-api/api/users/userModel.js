fcf3ddb956bc55dcc4d7815896f07461b2f4b956
Dylan <dylanchai2003@gmail.com>
2023-12-16 11:42:31 +0000

    Initilise Dev Env - Data

---
+++ movies-api/api/users/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(v);
+      },
+      message: props => `Password should be at least 8 characters long and contain at least one letter, one digit, and one special character.`
+    }
+  }
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
+  if (this.isModified('password') || this.isNew) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+export default mongoose.model('User', UserSchema);
